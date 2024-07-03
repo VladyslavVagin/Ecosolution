5ac1fb6454bb916e6e3983ad8120a4f1063e74d5
VladyslavVagin <140642012+VladyslavVagin@users.noreply.github.com>
2024-07-03 19:49:42 +0200

    add reset form

--- src/components/ContactUs/ContactForm/ContactForm.jsx
+++ src/components/ContactUs/ContactForm/ContactForm.jsx
@@ -11,55 +11,57 @@ const ContactForm = () => {
     register,
     handleSubmit,
     formState: { errors },
+    reset,
   } = useForm({ resolver: yupResolver(contactSchema), mode: "onBlur" });
 
   const onSubmit = (data) => {
     toast.success("Your message has been sent!");
+    reset();
   };
 
   return (
-      <Form onSubmit={handleSubmit(onSubmit)}>
-        <Label htmlFor="name">
-          *&nbsp;Full name:
-          <input
-            type="text"
-            {...register("name")}
-            placeholder="John Rosie"
-            style={{ borderBottomColor: errors.name && "var(--error-color)" }}
-          />
-          {errors.name && <ErrorMsg>{errors.name.message}</ErrorMsg>}
-        </Label>
-        <Label htmlFor="email">
-          *&nbsp;E-mail:
-          <input
-            type="email"
-            {...register("email")}
-            placeholder="johnrosie@gmail.com"
-            style={{ borderBottomColor: errors.email && "var(--error-color)" }}
-          />
-          {errors.email && <ErrorMsg>{errors.email.message}</ErrorMsg>}
-        </Label>
-        <Label htmlFor="phone">
-          *&nbsp;Phone:
-          <input
-            type="tel"
-            {...register("phone")}
-            placeholder="380961234567"
-            style={{ borderBottomColor: errors.phone && "var(--error-color)" }}
-          />
-          {errors.phone && <ErrorMsg>{errors.phone.message}</ErrorMsg>}
-        </Label>
-        <Label htmlFor="message">
-          Message:
-          <textarea {...register("message")} placeholder="Your message" />
-        </Label>
-        <SubmitBtn type="submit">
-          Send
-          <svg width={32} height={32}>
-            <use xlinkHref={`${sprite}#icon-arrow-up-right`}></use>
-          </svg>
-        </SubmitBtn>
-      </Form>
+    <Form onSubmit={handleSubmit(onSubmit)}>
+      <Label htmlFor="name">
+        *&nbsp;Full name:
+        <input
+          type="text"
+          {...register("name")}
+          placeholder="John Rosie"
+          style={{ borderBottomColor: errors.name && "var(--error-color)" }}
+        />
+        {errors.name && <ErrorMsg>{errors.name.message}</ErrorMsg>}
+      </Label>
+      <Label htmlFor="email">
+        *&nbsp;E-mail:
+        <input
+          type="email"
+          {...register("email")}
+          placeholder="johnrosie@gmail.com"
+          style={{ borderBottomColor: errors.email && "var(--error-color)" }}
+        />
+        {errors.email && <ErrorMsg>{errors.email.message}</ErrorMsg>}
+      </Label>
+      <Label htmlFor="phone">
+        *&nbsp;Phone:
+        <input
+          type="tel"
+          {...register("phone")}
+          placeholder="380961234567"
+          style={{ borderBottomColor: errors.phone && "var(--error-color)" }}
+        />
+        {errors.phone && <ErrorMsg>{errors.phone.message}</ErrorMsg>}
+      </Label>
+      <Label htmlFor="message">
+        Message:
+        <textarea {...register("message")} placeholder="Your message" />
+      </Label>
+      <SubmitBtn type="submit">
+        Send
+        <svg width={32} height={32}>
+          <use xlinkHref={`${sprite}#icon-arrow-up-right`}></use>
+        </svg>
+      </SubmitBtn>
+    </Form>
   );
 };
 
